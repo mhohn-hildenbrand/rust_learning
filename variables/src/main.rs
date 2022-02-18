75e576f3a422d612f96ec38c3cb9a622c990be6f
Michael Hohn <michael.hohn.hildenbrand@gmail.com>
2022-02-18 14:40:31 -0700

    Chapter 3.2 - Data Types

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -26,6 +26,75 @@ fn main() {
 			// Note that shadowing in a context leaves the original alone.
 	}
 
-	println!("The value of y is: {}", y)
+	println!("The value of y is: {}", y);
+
+	// Scalar Types - single values
+
+	// * Integer Types, signed and unsigned
+
+	let _myu8: u8 = 255;	
+		// the underscore supresses the warning that the unused variable is intentional?
+	let _myi8: i8 = -128;	
+	let _myu16: u16 = 65535;	
+	let _myi16: i16 = -32728;	
+	let _myu32: u32 = 4294967295;	
+	let _myi32: i32 = -2147483648;	
+	let _myu64: u64 = 18446744073709551615;	
+	let _myi64: i64 = -9223372036854775808;
+	let _myu128: u128 = 340282366920938463463374607431768211455;	
+	let _myi128: i128 = -170141183460469231731687303715884105728;	
+	let _myusize: usize = 18446744073709551615; // I'm working on a 64 bit machine
+	let _myisize: isize = -1;
+
+	//* Other forms of literals
+
+	let _my_decimal: u16 = 1_000; // Same as 1000
+	let _my_hex = 0xfe;
+	let _my_octal = 0o75;
+	let _my_binary = 0b1111_0000;
+	let _my_byte = b'A'; //only works in u8?
+
+	//* floating point tyoes
+
+	let _my_f64: f64 = 6.4;
+	let _my_f32: f32 = 3.2;
+
+	//* basic arithmetic
+
+	let _my_sum = 5 + 10;
+	let _my_difference = 95.5 - 4.3;
+	let _my_product = 4*30;
+	let _my_quotient = 56.7 / 32.2;
+	let _my_floored = 2/3; //results in 0 because the type is evaluated as int
+	let _my_remainder = 43 % 5;
+
+	//* Booleans
+
+	let _my_bool: bool = true;
+
+	//* Character
+
+	let _my_char: char = 'z'; // stores a unicode character
+
+	// Compound Types
+
+	// * Tuples
+
+	let _my_tuple: (i32, f64, char) = (500, 6.4, 'A');
+	let (_my_x, _my_y, _my_z) = _my_tuple;
+	println!("Using Tuple destructuring: {}", _my_y);
+	println!("Using Tuple indexing: {}", _my_tuple.1);
+
+	let _my_unit_value: () = (); //special type "unit type" that only has one value, (). this is the default return type of
+					// expressions that don't have explicit returns.
+
+	// * Arrays
+
+	let _my_array: [i32; 5] = [1, 2, 3, 4, 5];
+		// Allocates memory on the stack rather than the heap.
+	let _my_initialized_array = [ 7, 5 ]; // initialized an array of length 5 with value 7 in each place 
+
+	println!("Using Array indexing: {}", _my_array[2]);
+		// Note that using an out-of-bounds index will throw a runtime error
 
 }
